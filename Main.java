18110d3c6883fde93da42c3b902e495dd39fbbff
Brian Wong <brianwong@zipcoders-MacBook-Pro.local>
2019-02-05 15:47:46 -0500

    Finihed Alice and Bob Project

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
+        Scanner name = new Scanner(System.in);
+        System.out.println("What is your name?");
+        
+        String greetings = name.nextLine();
+        if(greetings.equals("Bob") || greetings.equals("Alice")) {
+            System.out.println("Hello " + greetings);}
+            else {
+                System.out.println("You are not the Bob or Alice");}
+        
     }
 }
